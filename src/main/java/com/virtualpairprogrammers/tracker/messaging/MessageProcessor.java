dacf3ef271f73f9289683859a93192ba0d156096
Mohamed Elalem <mohamed.el.alem.2017@gmail.com>
2022-02-20 19:51:58 -0600

    fix break

--- src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
+++ src/main/java/com/virtualpairprogrammers/tracker/messaging/MessageProcessor.java
@@ -32,7 +32,7 @@ public class MessageProcessor {
 																	.withName(incomingMessage.get("vehicle"))
 																	.withLat(new BigDecimal(incomingMessage.get("lat")))
 																	.withLng(new BigDecimal(incomingMessage.get("long")))
-																	.withSpeed(47.5)
+																	.withSpeed(BigDecimal.valueOf(47.5))
 																	.withTimestamp(convertedDatestamp)
 																	.build();
 				                          
